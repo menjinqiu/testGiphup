f4590426b2b906e4a74247ceb99999295bc83cb2
luorongjie <luorong_jie@foxmail.com>
2016-03-19 00:01:04 +0800

    update 01

--- src/testGiphup/HelloWorld.java
+++ src/testGiphup/HelloWorld.java
@@ -2,6 +2,8 @@ package testGiphup;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world1");
+		System.out.println("hello world2");
+		System.out.println("hello world3");
 	}
 }
